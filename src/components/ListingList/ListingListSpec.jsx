78b2d3fb61867366f3ee4d7c03a6fd7439d20a3a
Steven Zhang <steven.zhang@rea-group.com>
2017-06-03 18:00:30 +1000

    fix unit tests

--- src/components/ListingList/ListingListSpec.jsx
+++ src/components/ListingList/ListingListSpec.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import ListingList from './ListingList';
-import Listing from '../Listing';
+import ListingCard from './ListingCard';
 
 describe('ListingList Component', () => {
   const LISTING = {
-    agency: {
-      agencyName: 'agency-name',
-      logoUrl: 'logo-url',
-      backgroundColor: 'background-color',
+    branding: {
+      color: '#2c383d',
+      logo: {
+        url: 'https://i1.au.reastatic.net/agencylogo/DIMGNH/12/20160823153934.gif',
+        alt: 'Coronis - Coorparoo',
+      },
+    },
+    mainPhoto: {
+      url: 'https://i2.au.reastatic.net/640x480/4cd186cdfca5941d6a72e5a58b3b8bb1e490df705f38168e491524f3120b5c9f/main.jpg',
+      alt: '6/12 East Street, Camp Hill, Qld 4152',
+    },
+    propertyDetails: {
+      price: 'Offers Over $439,000',
+      address: '6/12 East Street, Camp Hill, Qld 4152',
+      bedrooms: 3,
+      bathrooms: 1,
+      carSpaces: 2,
     },
   };
   const PROPS = {
@@ -18,7 +31,7 @@ describe('ListingList Component', () => {
 
   it('should render a list of Listing component', () => {
     const wrapper = shallow(<ListingList {...PROPS} />);
-    const listings = wrapper.find(Listing);
+    const listings = wrapper.find(ListingCard);
 
     expect(listings).to.have.length(2);
     expect(listings.first().props()).to.eql(LISTING);
